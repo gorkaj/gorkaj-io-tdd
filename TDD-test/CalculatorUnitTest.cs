dea904ba9f2695bb80b68aa56c8213e272472576
gorkaj <01161318@pw.edu.pl>
2023-03-03 10:25:14 +0100

    Add test coverage for adding with StringCalculator

--- TDD-test/CalculatorUnitTest.cs
+++ TDD-test/CalculatorUnitTest.cs
@@ -4,17 +4,71 @@ namespace TDD_test
 {
     public class StringCalculatorUnitTest
     {
-        private readonly StringCalculator calculator;
+        private readonly StringCalculator _calculator;
         
         public StringCalculatorUnitTest()
         {
-            calculator = new();
+            _calculator = new StringCalculator();
         }
 
         [Fact]
         public void ShouldReturnZeroWhenEmptyStringPassed()
         {
-            Assert.Equal(0, calculator.Add(""));
+            Assert.Equal(0, _calculator.Add(""));
+        }
+
+        [Theory]
+        [InlineData("4", 4)]
+        [InlineData("0", 0)]
+        [InlineData("11", 11)]
+        public void ShouldReturnPassedNumberIfASingleNumberPassed(string numbers, int expected)
+        {
+            Assert.Equal(expected, _calculator.Add(numbers));
+        }
+
+        [Theory]
+        [InlineData("2,0", 2)]
+        [InlineData("1,7", 8)]
+        [InlineData("2,4", 6)]
+        public void ShouldReturnSumOfTwoNumbersDelimitedByComma(string numbers, int expected)
+        {
+            Assert.Equal(expected, _calculator.Add(numbers));
+        }
+        
+        [Theory]
+        [InlineData("2\n5", 7)]
+        [InlineData("1\n7", 8)]
+        [InlineData("2\n4", 6)]
+        public void ShouldReturnSumOfTwoNumbersDelimitedByNewLine(string numbers, int expected)
+        {
+            Assert.Equal(expected, _calculator.Add(numbers));
+        }
+        
+        [Theory]
+        [InlineData("2\n0,2", 4)]
+        [InlineData("1\n7\n3", 11)]
+        [InlineData("2,4,1", 7)]
+        public void ShouldReturnSumOfThreeNumbersDelimitedEitherWay(string numbers, int expected)
+        {
+            Assert.Equal(expected, _calculator.Add(numbers));
+        }
+        
+        [Theory]
+        [InlineData("2,-5")]
+        [InlineData("1\n7,-1")]
+        [InlineData("-2,-4,1")]
+        public void ShouldThrowAnExceptionWhenPassedNegativeNumber(string numbers)
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => _calculator.Add(numbers));
+        }
+        
+        [Theory]
+        [InlineData("2\n1001,2", 4)]
+        [InlineData("1\n7\n1000", 1008)]
+        [InlineData("2300,4,1", 5)]
+        public void ShouldIgnoreNumbersGreaterThanAThousand(string numbers, int expected)
+        {
+            Assert.Equal(expected, _calculator.Add(numbers));
         }
     }
 }
--- TDD/StringCalculator.cs
+++ TDD/StringCalculator.cs
@@ -1,11 +1,35 @@
-﻿
-namespace TDD
+﻿namespace TDD
 {
     public class StringCalculator
     {
         public int Add(string numbers)
         {
-            return 0;
+            if (string.IsNullOrEmpty(numbers))
+                return 0;
+
+            var nums = numbers.Split(',', '\n');
+            int sum = 0;
+            foreach (var n in nums)
+            {
+                try
+                {
+                    int num = int.Parse(n);
+                    switch (num)
+                    {
+                        case < 0:
+                            throw new ArgumentOutOfRangeException();
+                        case <= 1000:
+                            sum += num;
+                            break;
+                    }
+                }
+                catch (FormatException e)
+                {
+                    Console.Error.WriteLine(e);
+                    return 0;
+                }
+            }
+            return sum;
         }
     }
 }
